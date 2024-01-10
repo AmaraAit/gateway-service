49577f505fa3082efb8b5d0b63715469bec660df
amara <ait_amara@outlook.fr>
2024-01-10 05:09:34 +0100

    FC

---
+++ src/test/java/com/foot/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foot.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
